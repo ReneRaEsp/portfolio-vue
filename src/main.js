a38b852475e418ed145101a8c9db7326d8e28f4a
Rene Ramirez <rdejramirez@gmail.com>
2022-05-23 20:59:44 -0500

    portafolio en proceso

--- src/main.js
+++ src/main.js
@@ -1,18 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
 
-/* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* add icons to the library */
-library.add(faUserSecret);
-
-/* add font awesome icon component */
-
-createApp(App).use(router).mount('#app').component('font-awesome-icon', FontAwesomeIcon)
+createApp(App).use(router).mount('#app')
