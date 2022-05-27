d58a0eff8a54f37dc8fb35fb0f61ec3914c89d0e
Rene Ramirez <rdejramirez@gmail.com>
2022-05-27 14:30:32 -0500

    maquetacion responsive de el portafolio

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Seccion1 />
     <Seccion2 />
     <Tecnologias />    
+    <Portafolio />
   </div>
 </template>
 
@@ -10,9 +11,10 @@
 import Seccion1 from "./components/Seccion1.vue";
 import Seccion2 from "./components/Seccion2.vue";
 import Tecnologias from "./components/Tecnologias.vue";
+import Portafolio from "./components/Portafolio.vue";
 export default {
   components: {
-    Seccion1, Seccion2, Tecnologias
+    Seccion1, Seccion2, Tecnologias, Portafolio
   }
 }
 </script>
